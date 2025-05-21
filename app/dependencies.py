e3e0c3de07d0d390afa0a15e738241a58fd24304
Artem Shashov <tauquantum@gmail.com>
2025-05-21 15:14:58 +0900

    feat: added getting organization service dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,5 +4,13 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.postgres.connection import database
+from app.services.organization import OrganizationService
 
 SessionDep = Annotated[AsyncSession, Depends(database.get_session)]
+
+
+def get_organization_service(session: SessionDep) -> OrganizationService:
+    return OrganizationService(db_session=session)
+
+
+OrgServiceDep = Annotated[OrganizationService, Depends(get_organization_service)]
