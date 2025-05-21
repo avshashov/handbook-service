2b7743fe50f8b862b48828022ee43a355a5b6f0c
Artem Shashov <tauquantum@gmail.com>
2025-05-21 11:32:26 +0900

    feat: added db connection and orm models

---
+++ app/database/postgres/connection.py
@@ -0,0 +1,25 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from config import DatabaseSettings, settings
+
+
+class DBConnection:
+    def __init__(self, db_settings: DatabaseSettings) -> None:
+        self._settings = db_settings
+        self.engine = create_async_engine(
+            url=db_settings.get_url().encoded_string(),
+            echo=db_settings.debug,
+            pool_size=db_settings.pool_size,
+            max_overflow=db_settings.max_overflow,
+            pool_timeout=60,
+        )
+        self.async_session = async_sessionmaker(bind=self.engine, autoflush=False, expire_on_commit=False)
+
+    async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.async_session() as session:
+            yield session
+
+
+database = DBConnection(db_settings=settings)
---
+++ app/database/postgres/models/__init__.py
@@ -0,0 +1,5 @@
+from app.database.postgres.models.activity import Activity as Activity
+from app.database.postgres.models.building import Building as Building
+from app.database.postgres.models.organization import Organization as Organization
+from app.database.postgres.models.organization import organization_activity as organization_activity
+from app.database.postgres.models.phone import OrganizationPhone as OrganizationPhone
