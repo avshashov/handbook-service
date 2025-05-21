12f075a317d26b3ac60634db717c6aa6be2455b1
Artem Shashov <tauquantum@gmail.com>
2025-05-21 11:31:39 +0900

    feat: added project settings

---
+++ config.py
@@ -0,0 +1,43 @@
+import typing
+
+import yaml
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings
+
+
+class DatabaseSettings(BaseModel):
+    dbms: str
+    driver: str
+    host: str
+    port: int
+    user: str
+    password: str | None = None
+    database: str
+    pool_size: int
+    max_overflow: int
+    debug: bool = False
+
+    def get_url(self) -> PostgresDsn:
+        return PostgresDsn(
+            f'{self.dbms}+'
+            f'{self.driver}://'
+            f'{self.user}:'
+            f'{self.password or ""}@'
+            f'{self.host}:'
+            f'{self.port}/'
+            f'{self.database}'
+        )
+
+
+class Settings(BaseSettings):
+    database: DatabaseSettings
+
+    @classmethod
+    def from_yaml(cls, file_path: str) -> typing.Self:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = yaml.safe_load(file)
+
+        return cls(**data)
+
+
+settings = Settings.from_yaml('config.yaml')
